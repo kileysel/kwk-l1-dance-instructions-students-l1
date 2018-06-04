327cd836c49c3e0b9b685ac06b086ff38f62ffae
kileysel <kiley@selmanhouse.com>
2018-06-04 23:36:19 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,14 @@
-
+puts "STEP 1A: plant legs far apart, bend knees slightly and keep posture loose."
+puts "STEP1B: lift right foot."
+puts "STEP2B: return right foot to the ground."
+puts "STEP3B: finishing with a small skip-step backward."
+puts "STEP1C: lower left foot to the ground."
+puts "STEP2C: bounce left foot back up slightly, kicking it a few inches back."
+puts "STEP3C: lower right foot to the ground."
+puts "STEP1D: holding your arms out in front of you,straight and at chest level."
+puts "STEP2D: cross your right wrist over your left and hold them together."
+puts "STEP3D: lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+puts "STEP1E: start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+puts "STEP2E: lift right arm and point right elbow diagonally to the right."
+puts "STEP3E: raise right forearm so that it points straightup."
 # Code your methods below
