d66dff60be15e38751fc80d497382a65eafdb096
kileysel <kiley@selmanhouse.com>
2018-06-05 05:17:00 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -10,5 +10,5 @@ puts "STEP2D: cross your right wrist over your left and hold them together."
 puts "STEP3D: lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
 puts "STEP1E: start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
 puts "STEP2E: lift right arm and point right elbow diagonally to the right."
-puts "STEP3E: raise right forearm so that it points straightup."
-# Code your methods below
+puts "STEP3E: raise right forearm so that it points straightup, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+#Code your methods below
